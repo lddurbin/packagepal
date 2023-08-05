8c2e396cfeaaebe6ad250a99d0a4157ffc2f4870
lddurbin <l.d.durbin@gmail.com>
2023-08-05 22:22:44 +1200

    Re-declared package dependencies

--- R/packagepal-package.R
+++ R/packagepal-package.R
@@ -2,5 +2,14 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom available available
+#' @importFrom usethis ui_done
+#' @importFrom usethis ui_info
+#' @importFrom usethis ui_nope
+#' @importFrom usethis ui_oops
+#' @importFrom usethis ui_value
+#' @importFrom usethis ui_yeah
+#' @importFrom utils browseURL
+#' @importFrom utils install.packages
 ## usethis namespace: end
 NULL
